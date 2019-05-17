1d4ef27bf8d84b04c5703ee1a1a65abcd21fcefd
Denis Samuilik <denis.samuilik1990@gmail.com>
2019-05-17 15:03:50 +0300

    project initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from betmaster.skeleton import fib
+
+__author__ = "Denis Samuilik"
+__copyright__ = "Denis Samuilik"
+__license__ = "mit"
+
